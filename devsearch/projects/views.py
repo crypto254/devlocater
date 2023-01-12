9c90924f6a3f2957a8099af63dd730ebc4f76e90
unknown <kmuturierick@gmail.com>
2023-01-12 11:32:20 +0300

    first commit

---
+++ devsearch/projects/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+projectsList = [
+    {
+        'id': '1',
+        'title' : 'Ecommerce Website',
+        'description' : 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title' : 'Portfolio website',
+        'description' : 'A project where i built up my portfolio'
+    },
+    {
+        'id': '3',
+        'title' : 'Social Network',
+        'description' : 'Awesome open source project I am still working on'
+    }
+]
+
+def projects(request):
+    page = "Projects"
+    number = 10
+    
+
+    context = {
+        'page': page,
+        'number' : number,
+        'projects' : projectsList
+    }
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project':projectObj})
